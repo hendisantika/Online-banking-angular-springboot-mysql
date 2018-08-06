647d507a7fb0d6ad80959a18d41adf0c0e24390d
Hendi Santika <hendisantika@gmail.com>
2018-08-06 20:46:35 +0700

    #5 - Add Role Security

---
+++ src/main/java/com/hendisantika/onlinebanking/security/Role.java
@@ -0,0 +1,61 @@
+package com.hendisantika.onlinebanking.security;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : online-banking
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/08/18
+ * Time: 20.45
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+public class Role {
+
+    @Id
+    private int roleId;
+
+    private String name;
+
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<UserRole> userRoles = new HashSet<>();
+
+    public Role() {
+
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<UserRole> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(Set<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+
+}
