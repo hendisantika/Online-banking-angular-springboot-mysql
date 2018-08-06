4ff3c2d5961ad7c0123fa33d77180a9bc83657fc
Hendi Santika <hendisantika@gmail.com>
2018-08-06 20:39:58 +0700

    #2 - Add Initial Commit

---
+++ src/main/java/com/hendisantika/onlinebanking/OnlineBankingApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.onlinebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBankingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/hendisantika/onlinebanking/OnlineBankingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.onlinebanking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlineBankingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
