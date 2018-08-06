93c6bb9eba6ccf86e6f5b5d5a0e3d43c27837cad
Hendi Santika <hendisantika@gmail.com>
2018-08-07 06:51:06 +0700

    #9 - Add Primary Account

---
+++ src/main/java/com/hendisantika/onlinebanking/entity/PrimaryAccount.java
@@ -0,0 +1,71 @@
+package com.hendisantika.onlinebanking.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : online-banking
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/08/18
+ * Time: 06.49
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+public class PrimaryAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private int accountNumber;
+    private BigDecimal accountBalance;
+
+    @OneToMany(mappedBy = "primaryAccount", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<PrimaryTransaction> primaryTransactionList;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public BigDecimal getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(BigDecimal accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public List<PrimaryTransaction> getPrimaryTransactionList() {
+        return primaryTransactionList;
+    }
+
+    public void setPrimaryTransactionList(List<PrimaryTransaction> primaryTransactionList) {
+        this.primaryTransactionList = primaryTransactionList;
+    }
+
+
+}
