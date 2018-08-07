011af0af5726700e53690781b1e04800fb6245a2
Hendi Santika <hendisantika@gmail.com>
2018-08-08 06:05:06 +0700

    #16 - Add Primary Account DAO

---
+++ src/main/java/com/hendisantika/onlinebanking/repository/PrimaryAccountDao.java
@@ -0,0 +1,19 @@
+package com.hendisantika.onlinebanking.repository;
+
+import com.hendisantika.onlinebanking.entity.PrimaryAccount;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : online-banking
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/08/18
+ * Time: 06.04
+ * To change this template use File | Settings | File Templates.
+ */
+public interface PrimaryAccountDao extends CrudRepository<PrimaryAccount, Long> {
+
+    PrimaryAccount findByAccountNumber(int accountNumber);
+}
