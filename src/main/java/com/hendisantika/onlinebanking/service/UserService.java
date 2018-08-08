38742a4dcd716c9b2adbb8dab45d3bd9b2a36317
Hendi Santika <hendisantika@gmail.com>
2018-08-09 04:35:20 +0700

    #25 - Add User Service

---
+++ src/main/java/com/hendisantika/onlinebanking/service/UserService.java
@@ -0,0 +1,43 @@
+package com.hendisantika.onlinebanking.service;
+
+import com.hendisantika.onlinebanking.entity.User;
+import com.hendisantika.onlinebanking.security.UserRole;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : online-banking
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/08/18
+ * Time: 04.34
+ * To change this template use File | Settings | File Templates.
+ */
+public interface UserService {
+
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+
+    boolean checkUserExists(String username, String email);
+
+    boolean checkUsernameExists(String username);
+
+    boolean checkEmailExists(String email);
+
+    void save(User user);
+
+    User createUser(User user, Set<UserRole> userRoles);
+
+    User saveUser(User user);
+
+    List<User> findUserList();
+
+    void enableUser(String username);
+
+    void disableUser(String username);
+
+}
