dc14c89234bc963ad04cfbda32430ac3f5800c33
Hendi Santika <hendisantika@gmail.com>
2018-08-09 04:34:38 +0700

    #24 - Add Transaction Service

---
+++ src/main/java/com/hendisantika/onlinebanking/service/TransactionService.java
@@ -0,0 +1,47 @@
+package com.hendisantika.onlinebanking.service;
+
+import com.hendisantika.onlinebanking.entity.PrimaryAccount;
+import com.hendisantika.onlinebanking.entity.PrimaryTransaction;
+import com.hendisantika.onlinebanking.entity.Recipient;
+import com.hendisantika.onlinebanking.entity.SavingsAccount;
+import com.hendisantika.onlinebanking.entity.SavingsTransaction;
+
+import java.security.Principal;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : online-banking
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/08/18
+ * Time: 04.34
+ * To change this template use File | Settings | File Templates.
+ */
+public interface TransactionService {
+
+    List<PrimaryTransaction> findPrimaryTransactionList(String username);
+
+    List<SavingsTransaction> findSavingsTransactionList(String username);
+
+    void savePrimaryDepositTransaction(PrimaryTransaction primaryTransaction);
+
+    void saveSavingsDepositTransaction(SavingsTransaction savingsTransaction);
+
+    void savePrimaryWithdrawTransaction(PrimaryTransaction primaryTransaction);
+
+    void saveSavingsWithdrawTransaction(SavingsTransaction savingsTransaction);
+
+    void betweenAccountsTransfer(String transferFrom, String transferTo, String amount, PrimaryAccount primaryAccount, SavingsAccount savingsAccount) throws Exception;
+
+    List<Recipient> findRecipientList(Principal principal);
+
+    Recipient saveRecipient(Recipient recipient);
+
+    Recipient findRecipientByName(String recipientName);
+
+    void deleteRecipientByName(String recipientName);
+
+    void toSomeoneElseTransfer(Recipient recipient, String accountType, String amount, PrimaryAccount primaryAccount, SavingsAccount savingsAccount);
+}
