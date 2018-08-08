740e24dba5cfab775eda2500967896f83445adfe
Hendi Santika <hendisantika@gmail.com>
2018-08-09 04:33:58 +0700

    #23 - Add Appointment Service

---
+++ src/main/java/com/hendisantika/onlinebanking/service/AppointmentService.java
@@ -0,0 +1,26 @@
+package com.hendisantika.onlinebanking.service;
+
+import com.hendisantika.onlinebanking.entity.Appointment;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : online-banking
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/08/18
+ * Time: 04.33
+ * To change this template use File | Settings | File Templates.
+ */
+public interface AppointmentService {
+
+    Appointment createAppointment(Appointment appointment);
+
+    List<Appointment> findAll();
+
+    Appointment findAppointment(Long id);
+
+    void confirmAppointment(Long id);
+}
