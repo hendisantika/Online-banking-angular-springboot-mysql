fc60920895dc987da6363b76df2f38c3d7963bf4
Hendi Santika <hendisantika@gmail.com>
2018-08-09 04:32:59 +0700

    #22 - Add Appointment Service

---
+++ src/main/java/com/hendisantika/onlinebanking/resource/AppointmentResource.java
@@ -0,0 +1,39 @@
+package com.hendisantika.onlinebanking.resource;
+
+import com.hendisantika.onlinebanking.entity.Appointment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : online-banking
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/08/18
+ * Time: 04.31
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/api/appointment")
+@PreAuthorize("hasRole('ADMIN')")
+public class AppointmentResource {
+
+    @Autowired
+    private AppointmentService appointmentService;
+
+    @RequestMapping("/all")
+    public List<Appointment> findAppointmentList() {
+        List<Appointment> appointmentList = appointmentService.findAll();
+
+        return appointmentList;
+    }
+
+    @RequestMapping("/{id}/confirm")
+    public void confirmAppointment(@PathVariable("id") Long id) {
+        appointmentService.confirmAppointment(id);
+    }
+}
---
+++ src/main/java/com/hendisantika/onlinebanking/service/AccountService.java
@@ -0,0 +1,28 @@
+package com.hendisantika.onlinebanking.service;
+
+import com.hendisantika.onlinebanking.entity.PrimaryAccount;
+import com.hendisantika.onlinebanking.entity.SavingsAccount;
+
+import java.security.Principal;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : online-banking
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/08/18
+ * Time: 04.32
+ * To change this template use File | Settings | File Templates.
+ */
+public interface AccountService {
+
+    PrimaryAccount createPrimaryAccount();
+
+    SavingsAccount createSavingsAccount();
+
+    void deposit(String accountType, double amount, Principal principal);
+
+    void withdraw(String accountType, double amount, Principal principal);
+
+}
