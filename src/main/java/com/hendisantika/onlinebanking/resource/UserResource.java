4e545f992756ac87e9c563c129a886444fd2fbfc
Hendi Santika <hendisantika@gmail.com>
2018-09-04 06:40:47 +0700

    #58 - Add User Resource

---
+++ src/main/java/com/hendisantika/onlinebanking/resource/UserResource.java
@@ -0,0 +1,59 @@
+package com.hendisantika.onlinebanking.resource;
+
+import com.hendisantika.onlinebanking.entity.PrimaryTransaction;
+import com.hendisantika.onlinebanking.entity.SavingsTransaction;
+import com.hendisantika.onlinebanking.entity.User;
+import com.hendisantika.onlinebanking.service.TransactionService;
+import com.hendisantika.onlinebanking.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : online-banking
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/09/18
+ * Time: 06.39
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/api")
+@PreAuthorize("hasRole('ADMIN')")
+public class UserResource {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @RequestMapping(value = "/user/all", method = RequestMethod.GET)
+    public List<User> userList() {
+        return userService.findUserList();
+    }
+
+    @RequestMapping(value = "/user/primary/transaction", method = RequestMethod.GET)
+    public List<PrimaryTransaction> getPrimaryTransactionList(@RequestParam("username") String username) {
+        return transactionService.findPrimaryTransactionList(username);
+    }
+
+    @RequestMapping(value = "/user/savings/transaction", method = RequestMethod.GET)
+    public List<SavingsTransaction> getSavingsTransactionList(@RequestParam("username") String username) {
+        return transactionService.findSavingsTransactionList(username);
+    }
+
+    @RequestMapping("/user/{username}/enable")
+    public void enableUser(@PathVariable("username") String username) {
+        userService.enableUser(username);
+    }
+
+    @RequestMapping("/user/{username}/disable")
+    public void diableUser(@PathVariable("username") String username) {
+        userService.disableUser(username);
+    }
+}
