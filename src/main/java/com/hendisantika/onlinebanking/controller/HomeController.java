baf406e66eaea3d831b535a621d43eea3a863b91
Hendi Santika <hendisantika@gmail.com>
2018-09-04 06:35:26 +0700

    #54 - Add Home Controller

---
+++ src/main/java/com/hendisantika/onlinebanking/controller/HomeController.java
@@ -0,0 +1,93 @@
+package com.hendisantika.onlinebanking.controller;
+
+import com.hendisantika.onlinebanking.entity.PrimaryAccount;
+import com.hendisantika.onlinebanking.entity.SavingsAccount;
+import com.hendisantika.onlinebanking.entity.User;
+import com.hendisantika.onlinebanking.repository.RoleDao;
+import com.hendisantika.onlinebanking.security.UserRole;
+import com.hendisantika.onlinebanking.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.security.Principal;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : online-banking
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/09/18
+ * Time: 06.34
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class HomeController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RoleDao roleDao;
+
+    @RequestMapping("/")
+    public String home() {
+        return "redirect:/index";
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(Model model) {
+        User user = new User();
+
+        model.addAttribute("user", user);
+
+        return "signup";
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public String signupPost(@ModelAttribute("user") User user, Model model) {
+
+        if (userService.checkUserExists(user.getUsername(), user.getEmail())) {
+
+            if (userService.checkEmailExists(user.getEmail())) {
+                model.addAttribute("emailExists", true);
+            }
+
+            if (userService.checkUsernameExists(user.getUsername())) {
+                model.addAttribute("usernameExists", true);
+            }
+
+            return "signup";
+        } else {
+            Set<UserRole> userRoles = new HashSet<>();
+            userRoles.add(new UserRole(user, roleDao.findByName("ROLE_USER")));
+
+            userService.createUser(user, userRoles);
+
+            return "redirect:/";
+        }
+    }
+
+    @RequestMapping("/userFront")
+    public String userFront(Principal principal, Model model) {
+        User user = userService.findByUsername(principal.getName());
+        PrimaryAccount primaryAccount = user.getPrimaryAccount();
+        SavingsAccount savingsAccount = user.getSavingsAccount();
+
+        model.addAttribute("primaryAccount", primaryAccount);
+        model.addAttribute("savingsAccount", savingsAccount);
+
+        return "userFront";
+    }
+}
