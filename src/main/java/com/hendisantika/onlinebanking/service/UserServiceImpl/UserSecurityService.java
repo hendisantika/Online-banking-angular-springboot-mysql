5ecbeed878c5916d437711aa0fb6a6a2dff1701b
Hendi Santika <hendisantika@gmail.com>
2018-09-04 06:31:39 +0700

    #51 - Add User Security Service

---
+++ src/main/java/com/hendisantika/onlinebanking/service/UserServiceImpl/UserSecurityService.java
@@ -0,0 +1,40 @@
+package com.hendisantika.onlinebanking.service.UserServiceImpl;
+
+import com.hendisantika.onlinebanking.entity.User;
+import com.hendisantika.onlinebanking.repository.UserDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : online-banking
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/09/18
+ * Time: 06.30
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class UserSecurityService implements UserDetailsService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserSecurityService.class);
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userDao.findByUsername(username);
+        if (null == user) {
+            LOG.warn("Username {} not found", username);
+            throw new UsernameNotFoundException("Username " + username + " not found");
+        }
+        return user;
+    }
+}
