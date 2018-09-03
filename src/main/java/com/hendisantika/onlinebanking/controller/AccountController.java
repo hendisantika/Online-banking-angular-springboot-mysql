1d12d0065c4414cbd75ec9756573b194270bd05e
Hendi Santika <hendisantika@gmail.com>
2018-09-04 06:33:57 +0700

    #53 - Add Account Controller

---
+++ src/main/java/com/hendisantika/onlinebanking/controller/AccountController.java
@@ -0,0 +1,94 @@
+package com.hendisantika.onlinebanking.controller;
+
+import com.hendisantika.onlinebanking.entity.*;
+import com.hendisantika.onlinebanking.service.AccountService;
+import com.hendisantika.onlinebanking.service.TransactionService;
+import com.hendisantika.onlinebanking.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.security.Principal;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : online-banking
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/09/18
+ * Time: 06.32
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @RequestMapping("/primaryAccount")
+    public String primaryAccount(Model model, Principal principal) {
+        List<PrimaryTransaction> primaryTransactionList = transactionService.findPrimaryTransactionList(principal.getName());
+
+        User user = userService.findByUsername(principal.getName());
+        PrimaryAccount primaryAccount = user.getPrimaryAccount();
+
+        model.addAttribute("primaryAccount", primaryAccount);
+        model.addAttribute("primaryTransactionList", primaryTransactionList);
+
+        return "primaryAccount";
+    }
+
+    @RequestMapping("/savingsAccount")
+    public String savingsAccount(Model model, Principal principal) {
+        List<SavingsTransaction> savingsTransactionList = transactionService.findSavingsTransactionList(principal.getName());
+        User user = userService.findByUsername(principal.getName());
+        SavingsAccount savingsAccount = user.getSavingsAccount();
+
+        model.addAttribute("savingsAccount", savingsAccount);
+        model.addAttribute("savingsTransactionList", savingsTransactionList);
+
+        return "savingsAccount";
+    }
+
+    @RequestMapping(value = "/deposit", method = RequestMethod.GET)
+    public String deposit(Model model) {
+        model.addAttribute("accountType", "");
+        model.addAttribute("amount", "");
+
+        return "deposit";
+    }
+
+    @RequestMapping(value = "/deposit", method = RequestMethod.POST)
+    public String depositPOST(@ModelAttribute("amount") String amount, @ModelAttribute("accountType") String accountType, Principal principal) {
+        accountService.deposit(accountType, Double.parseDouble(amount), principal);
+
+        return "redirect:/userFront";
+    }
+
+    @RequestMapping(value = "/withdraw", method = RequestMethod.GET)
+    public String withdraw(Model model) {
+        model.addAttribute("accountType", "");
+        model.addAttribute("amount", "");
+
+        return "withdraw";
+    }
+
+    @RequestMapping(value = "/withdraw", method = RequestMethod.POST)
+    public String withdrawPOST(@ModelAttribute("amount") String amount, @ModelAttribute("accountType") String accountType, Principal principal) {
+        accountService.withdraw(accountType, Double.parseDouble(amount), principal);
+
+        return "redirect:/userFront";
+    }
+}
