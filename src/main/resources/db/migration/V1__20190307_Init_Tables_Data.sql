6cea048f23b1c4450959b9be13e843c7908ca474
hendisantika <hendisantika@yahoo.co.id>
2019-03-07 09:23:19 +0700

    #69 - BUg Fix Migration Script file

--- src/main/resources/db/migration/V1__20190307_Init_Tables_Data.sql
+++ src/main/resources/db/migration/V1__20190307_Init_Tables_Data.sql
@@ -43,7 +43,8 @@ CREATE TABLE `appointment` (
 
 LOCK TABLES `appointment` WRITE;
 /*!40000 ALTER TABLE `appointment` DISABLE KEYS */;
-INSERT INTO `appointment` VALUES (1,'','2019-01-25 14:01:00','Want to see someone','Jakarta',1),(2,'\0','2019-01-30 15:01:00','Take credit','Jakarta',1),(3,'','2019-02-16 15:02:00','Consultation','Jakarta',1);
+INSERT INTO `appointment` VALUES (1,'','2019-01-25 14:01:00','Want to see someone','Jakarta',1),
+(2,'\0','2019-01-30 15:01:00','Take credit','Jakarta',1),(3,'','2019-02-16 15:02:00','Consultation','Jakarta',1);
 /*!40000 ALTER TABLE `appointment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -100,7 +101,14 @@ CREATE TABLE `primary_transaction` (
 
 LOCK TABLES `primary_transaction` WRITE;
 /*!40000 ALTER TABLE `primary_transaction` DISABLE KEYS */;
-INSERT INTO `primary_transaction` VALUES (1,5000,5000.00,'2019-01-13 00:57:16','Deposit to Primary Account','Finished','Account',1),(2,1500,3500.00,'2019-01-13 00:57:31','Withdraw from Primary Account','Finished','Account',1),(3,1300,2200.00,'2019-01-13 00:58:03','Between account transfer from Primary to Savings','Finished','Account',1),(4,500,1700.00,'2019-01-13 00:59:08','Transfer to recipient Mr. Tomson','Finished','Transfer',1),(5,1500,3200.00,'2019-01-13 01:11:38','Deposit to Primary Account','Finished','Account',1),(6,400,2800.00,'2019-01-13 01:11:46','Withdraw from Primary Account','Finished','Account',1),(7,2300,2000.00,'2019-01-13 01:13:48','Between account transfer from Primary to Savings','Finished','Account',1),(8,300,1700.00,'2019-01-13 01:14:14','Transfer to recipient TaxSystem','Finished','Transfer',1);
+INSERT INTO `primary_transaction` VALUES (1,5000,5000.00,'2019-01-13 00:57:16','Deposit to Primary Account','Finished','Account',1),
+(2,1500,3500.00,'2019-01-13 00:57:31','Withdraw from Primary Account','Finished','Account',1),
+(3,1300,2200.00,'2019-01-13 00:58:03','Between account transfer from Primary to Savings','Finished','Account',1),
+(4,500,1700.00,'2019-01-13 00:59:08','Transfer to recipient Mr. Tomson','Finished','Transfer',1),
+(5,1500,3200.00,'2019-01-13 01:11:38','Deposit to Primary Account','Finished','Account',1),
+(6,400,2800.00,'2019-01-13 01:11:46','Withdraw from Primary Account','Finished','Account',1),
+(7,2300,2000.00,'2019-01-13 01:13:48','Between account transfer from Primary to Savings','Finished','Account',1),
+(8,300,1700.00,'2019-01-13 01:14:14','Transfer to recipient TaxSystem','Finished','Transfer',1);
 /*!40000 ALTER TABLE `primary_transaction` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -131,7 +139,10 @@ CREATE TABLE `recipient` (
 
 LOCK TABLES `recipient` WRITE;
 /*!40000 ALTER TABLE `recipient` DISABLE KEYS */;
-INSERT INTO `recipient` VALUES (1,'213425635454','Rent payment','tomson@gmail.com','Mr. Tomson','1112223333',1),(2,'453452341324','Gym payment','fitness@gmail.com','LtdFitness','323245345',1),(3,'5465464234542','Tax payment 20%','taxes@mail.fi','TaxSystem','34254353',1);
+INSERT INTO `recipient` VALUES
+(1,'213425635454','Rent payment','tomson@gmail.com','Mr. Tomson','1112223333',1),
+(2,'453452341324','Gym payment','fitness@gmail.com','LtdFitness','323245345',1),
+(3,'5465464234542','Tax payment 20%','taxes@mail.fi','TaxSystem','34254353',1);
 /*!40000 ALTER TABLE `recipient` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -212,7 +223,13 @@ CREATE TABLE `savings_transaction` (
 
 LOCK TABLES `savings_transaction` WRITE;
 /*!40000 ALTER TABLE `savings_transaction` DISABLE KEYS */;
-INSERT INTO `savings_transaction` VALUES (1,1000,1000.00,'2019-01-13 00:57:40','Deposit to savings Account','Finished','Account',1),(2,150,2150.00,'2019-01-13 01:11:15','Withdraw from savings Account','Finished','Account',1),(3,400,1750.00,'2019-01-13 01:11:23','Withdraw from savings Account','Finished','Account',1),(4,2000,3750.00,'2019-01-13 01:11:30','Deposit to savings Account','Finished','Account',1),(5,1500,2250.00,'2019-01-13 01:13:38','Between account transfer from Savings to Primary','Finished','Transfer',1),(6,300,4250.00,'2019-01-13 01:14:02','Transfer to recipient LtdFitness','Finished','Transfer',1);
+INSERT INTO `savings_transaction` VALUES
+(1,1000,1000.00,'2019-01-13 00:57:40','Deposit to savings Account','Finished','Account',1),
+(2,150,2150.00,'2019-01-13 01:11:15','Withdraw from savings Account','Finished','Account',1),
+(3,400,1750.00,'2019-01-13 01:11:23','Withdraw from savings Account','Finished','Account',1),
+(4,2000,3750.00,'2019-01-13 01:11:30','Deposit to savings Account','Finished','Account',1),
+(5,1500,2250.00,'2019-01-13 01:13:38','Between account transfer from Savings to Primary','Finished','Transfer',1),
+(6,300,4250.00,'2019-01-13 01:14:02','Transfer to recipient LtdFitness','Finished','Transfer',1);
 /*!40000 ALTER TABLE `savings_transaction` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -249,7 +266,9 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'dleskov@gmail.ru','','Dmitry','Leskov','$2a$12$DWCryEwHwbTYCegib92tk.VST.GG1i2WAqfaSwyMdxX0cl0eBeSve','5551112345','User',1,1),(3,'asd@gmail.com','','alex','osipov','$2a$12$hZR7pcSf0JU/OTXR3TOyuu8r6C6n.JZE8Ei47E4LZs1t0Aq1AO6oC','1111111111','Admin',3,3);
+INSERT INTO `user` VALUES
+(1,'user@gmail.com','','Uchiha','Sasuke','$2a$12$DWCryEwHwbTYCegib92tk.VST.GG1i2WAqfaSwyMdxX0cl0eBeSve','5551112345','User',1,1),
+(2,'admin@gmail.com','','Uzumaki','Naruto','$2a$12$hZR7pcSf0JU/OTXR3TOyuu8r6C6n.JZE8Ei47E4LZs1t0Aq1AO6oC','1111111111','Admin',2,2);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -278,7 +297,7 @@ CREATE TABLE `user_role` (
 
 LOCK TABLES `user_role` WRITE;
 /*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
-INSERT INTO `user_role` VALUES (1,0,1),(2,1,3);
+INSERT INTO `user_role` VALUES (1,0,1),(2,1,2);
 /*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
