4969f640aa4bb01d937fe78174a29a66ab768768
hendisantika <hendisantika@yahoo.co.id>
2024-05-24 08:01:12 +0700

    #205 - Add Testcontainer libs :five:🫸🌀✏️📗🐧🐳

--- src/test/java/com/hendisantika/onlinebanking/OnlineBankingApplicationTests.java
+++ src/test/java/com/hendisantika/onlinebanking/OnlineBankingApplicationTests.java
@@ -1,13 +1,28 @@
 package com.hendisantika.onlinebanking;
 
-import org.junit.jupiter.api.Test;
+import com.hendisantika.onlinebanking.repository.UserDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@Testcontainers
+@SpringBootTest(
+        properties = {
+                "management.endpoint.health.show-details=always",
+                "spring.datasource.url=jdbc:tc:mysql:8.4.0:///bankDB"
+        },
+        webEnvironment = RANDOM_PORT
+)
 public class OnlineBankingApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+    @Autowired
+    private UserDao userRepository;
 
+    @BeforeEach
+    void deleteAll() {
+        userRepository.deleteAll();
+    }
 }
